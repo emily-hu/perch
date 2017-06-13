98163204a4b7f2bc90cb7852a966dc4814193b8b
Emily Hu <emilyhu@mit.edu>
2017-06-13 15:16:23 -0500

    Rename db and routing files

--- database/orgs.py
+++ database/orgs.py
@@ -2,19 +2,19 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-def new_org(org_name):
+def create_org(org_name):
     return "ok"
 
-def get_org_admins(org_name):
+def get_admins(org_name):
     return ["emilyhu", "tcqin"]
 
-def get_org_users(org_name):
+def get_users(org_name):
     return ["emilyhu", "tcqin", "gdu"]
 
-def add_org_admin(org_name, user_id):
+def add_admin(org_name, user_id):
     return "ok"
 
-def add_org_user(org_name, user_id):
+def add_user(org_name, user_id):
     return "ok"
 
 def delete_org(org_name):
--- routing/users.py
+++ routing/users.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 def create_user(): # name, email, password
     pass # return json
 
-@app.route('/users/<user_id>/name/', methods['GET'])
+@app.route('/users/<user_id>/name/', methods=['GET'])
 def get_user_name(user_id):
     name = get_name(user_id)
     if len(name) == 0:
         abort(404)
     return name
 
-@app.route('/users/<user_id>/orgs/', methods['GET'])
+@app.route('/users/<user_id>/orgs/', methods=['GET'])
 def get_user_orgs(user_id):
     org_list = get_orgs(user_id)
     name = get_name(user_id)
@@ -21,6 +21,6 @@ def get_user_orgs(user_id):
         abort(404)
     return org_list
 
-@app.route('/users/<user_id>/password/', methods['PUT'])
+@app.route('/users/<user_id>/password/', methods=['PUT'])
 def change_password(user_id): # new password
     pass # return success
