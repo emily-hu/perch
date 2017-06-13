072b6d4b7a7f49c9736c2dbae28a5f7aa2779992
Emily Hu <emilyhu@mit.edu>
2017-06-13 01:02:22 -0500

    Add initial database stubs

---
+++ database/org_stubs.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+def new_org(org_name):
+    return "ok"
+
+def get_org_admins(org_name):
+    return ["emilyhu", "tcqin"]
+
+def get_org_users(org_name):
+    return ["emilyhu", "tcqin", "gdu"]
+
+def add_org_admin(org_name, user_id):
+    return "ok"
+
+def add_org_user(org_name, user_id):
+    return "ok"
+
+def delete_org(org_name):
+    return "ok"
---
+++ database/user_stubs.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+def get_name(user_id):
+    return "Emily Hu"
+
+def get_orgs(user_id):
+    return ["Syncopasian", "ADT"]
