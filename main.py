33f2797931f90e20ddb11d8fb834fe6bfdcd1708
Emily Hu <emilyhu@mit.edu>
2017-06-13 01:01:17 -0500

    Add user authentication to main.py

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session, url_for, request, escape
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
+    # return render_template('index.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('/'))
+    return render_template('signup.html')
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect('/')
+
+app.secret_key = 'jdkaslti543y8uarief'
